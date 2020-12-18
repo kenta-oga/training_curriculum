af0a499920d38e04a5451e888ffa466b2146fc3f
kenta-oga <kenta555phi@gmail.com>
2020-12-18 21:26:26 +0900

    「wday_numが7以上の場合」という条件式 追加

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -35,7 +35,7 @@ class CalendarsController < ApplicationController
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
       wday_num = @todays_date.wday
-      if #「wday_numが7以上の場合」という条件式
+      if wday_num + x >= 7
         wday_num = wday_num -7
       end
       @wday = wdays[wday_num + x]
